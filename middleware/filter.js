23672430bbc9e17aa2928800839b08c846af7363
wuyuanwuhui99 <275018723@qq.com>
2021-03-13 23:19:08 +0800

    修改拦截器

--- middleware/filter.js
+++ middleware/filter.js
@@ -1,27 +1,32 @@
 const jsonwebtoken = require("jsonwebtoken");
-
+const {FAIL} = require("../config")
 //拦截器
 module.exports =  async (ctx,next) => {
     //添加收藏和取消收藏，获取收藏列表需要登录
-    const allowpage = ['/addFavorite','/getFavorite',"/deleteFavorite","/updateUser"]
+    const allowpage = ['/addFavorite','/getFavorite',"/deleteFavorite","/updateUser","/updatePassword"]
     let url = ctx.originalUrl
     for(let i = 0; i < allowpage.length; i++){
         if(ctx.originalUrl.indexOf(allowpage[i])!=-1){
             let token = ctx.cookies.get("token");
             if(token){
                 var userData = jsonwebtoken.decode(token);
-                if(userData){
-
+                if(!userData){
+                    ctx.status = 403;//返回状态
+                    ctx.body = {
+                        ...FAIL,
+                        msg:"未登录",
+                        data:null
+                    }
+                    return ;
                 }
-                ctx.body = {
 
-                    msg:"未登录"
-                }
             }else{
+                ctx.status = 403;//返回状态
                 ctx.body = {
-                    msg:"未登录"
+                    ...FAIL,
+                    msg:"未登录",
+                    data:null
                 }
-                ctx.status = 403;
                 return ;
             }
         }
